be4face17c8c6ec336eb286b5fd65a2a4d574685
tdoaty <tdoaty@google.com>
2017-03-21 17:10:40 -0400

    completed exercise

--- cartoon_collections.js
+++ cartoon_collections.js
@@ -1,27 +1,49 @@
+// Function 1 //
 function dwarfRollCall(dwarves) {
-  var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  dwarves.forEach(function(dwarf, index){
-    var number = index + 1;
-    console.log(number + "." + dwarf);
-    return dwarves;
+    var finalString = ""
+    dwarves.forEach(function(dwarf, index){
+       var number = index + 1;
+       finalString = finalString + number + "." + dwarf + " "
   });
+  return finalString
 }
 
+dwarfRollCall("Doc", "Dopey", "Bashful", "Grumpy")
+
+/*Function 2 */
 function summonCaptainPlanet(planeteerCalls){
-  var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+  var callString = ""
   planeteerCalls.forEach(function(element, index){
-    console.log(element.toUpperCase() + "!");
-    return planeteerCalls
+    callString = callString + element.toUpperCase() + "!"+ " "
   });
+  return callString
 }
 
+summonCaptainPlanet(["earth", "wind", "fire", "water", "heart"])
+
+// Function 3 //
 function longPlaneteerCalls(words) {
-  var words = ["earth", "wind", "fire", "water", "heart"]
-  for (var shortWords = "wind", "fire"; shortWords<4)
-  {
-    if (shortWords
-  }
-  for (var assortedWords = words;
+  var hasFiveOrMore = false;
+  words.forEach(function(word){
+    if(word.length >= 5){
+      hasFiveOrMore= true
+    }
+  });
+  return hasFiveOrMore
+}
+
+longPlaneteerCalls(["earth", "wind", "heart", "fire"])
+
+
+// Function 4 //
 function findTheCheese(foods) {
-	// your code here!
+  var cheeses = ["cheddar", "gouda", "camembert"]
+  finalCheese = "no cheese"
+  foods.forEach(function(food){
+    if(cheeses.includes(food))
+    {
+      finalCheese= food;
+    }
+  });
+  return finalCheese;
 }
